2bcbd954ce5397db57e17c0446e974b612c870ee
anuzza <anuja3580@gmail.com>
2022-07-06 04:44:39 -0500

    used Dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,19 @@
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES=[
+    {
+        id: 'q1',
+        author: 'Jones',
+        text: 'Learning react is fun!'
+    
+    },
+    {
+        id: 'q2',
+        author: 'David',
+        text: 'Learning React is Great!'
+    },
+];
 const AllQuotes = ()=>{
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes = {DUMMY_QUOTES}/>
 }
 
 export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,10 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = ()=>{
-    return <h1>New Quote Page</h1>
+    const addQuoteHandler = quoteData =>{
+     console.log(quoteData);   
+    }
+    return (<QuoteForm onAdd={addQuoteHandler}/>);
 }
 
 export default NewQuote;
