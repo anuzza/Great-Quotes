546ec2f962e80d1d292d8a97e93d07fc3ba36b02
anuzza <anuja3580@gmail.com>
2022-07-06 03:23:52 -0500

    used nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = ()=>{
     const params = useParams();
     return <section>
         <h1>Quote Details Page</h1>
         <p>{params.quoteId}</p>
+        <Route path = {`/quotes/${params.quoteId}/comments`}>
+            <Comments/>
+        </Route>
     </section>
     
 }
