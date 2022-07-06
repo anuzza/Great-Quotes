ab9a253eae57a135736bae1bb9af2f655f0c3812
anuzza <anuja3580@gmail.com>
2022-07-06 05:12:09 -0500

    implemented programmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,14 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = ()=>{
-    const addQuoteHandler = quoteData =>{
+    const history = useHistory();
+    const addQuoteHandler = (quoteData) =>{
      console.log(quoteData);   
-    }
-    return (<QuoteForm onAdd={addQuoteHandler}/>);
+     history.push('/quotes');
+    };
+
+    return (<QuoteForm onAddQuote={addQuoteHandler}/>);
 }
 
 export default NewQuote;
