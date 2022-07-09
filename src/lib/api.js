a321c96fc94c694ff5737e0b20167013166a15a6
anuzza <anuja3580@gmail.com>
2022-07-08 20:58:46 -0500

    sent requested data

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-quote-b6a10-default-rtdb.firebaseio.com/';
+const FIREBASE_DOMAIN = 'https://react-quote-b6a10-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
